e5889a2e4742af7ba3a284a3beec53c6e1468ac8
theani7 <theanilpaneru@gmail.com>
2026-07-17 08:55:18 +0545

    fix: include ts and tsx in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['src/**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
