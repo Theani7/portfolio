ff0bce1816fd985a9e5d997436ccf6cff858c66c
theani7 <theanilpaneru@gmail.com>
2026-07-17 08:53:56 +0545

    feat: migrate to typescript and add image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer()],
   build: {
     // Ensure proper asset hashing for cache busting
     rollupOptions: {
